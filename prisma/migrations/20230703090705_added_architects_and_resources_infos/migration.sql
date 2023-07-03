e4cb57ac6b5c6c486e0f2e8af909ee22aac70441
Hendrik Wichern <git@hendrikwichern.de>
2023-07-03 18:08:32 +0900

    added columns

---
+++ prisma/migrations/20230703090705_added_architects_and_resources_infos/migration.sql
@@ -0,0 +1,6 @@
+-- AlterTable
+ALTER TABLE "Architects" ADD COLUMN     "description" STRING;
+ALTER TABLE "Architects" ADD COLUMN     "url" STRING;
+
+-- AlterTable
+ALTER TABLE "Resources" ADD COLUMN     "copyright" STRING;
