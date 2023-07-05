eccc880269f491f63d5fb259413f4ca026a1cd22
Hendrik Wichern <git@hendrikwichern.de>
2023-07-05 12:02:53 +0900

    updated maps api key

--- src/components/admin/settlements/Map.tsx
+++ src/components/admin/settlements/Map.tsx
@@ -7,7 +7,7 @@ export function Map({ lat, lng }: { lat: number, lng: number }) {
         loading="lazy"
         allowFullScreen
         referrerPolicy="no-referrer-when-downgrade"
-        src={`https://www.google.com/maps/embed/v1/place?key=AIzaSyCRD4mqrbmf0ah6tGx-dGmjX02mztam738&q=${String(lat)},${String(lng)}`}>
+        src={`https://www.google.com/maps/embed/v1/place?key=${process.env.MAPS_API_KEY}&q=${String(lat)},${String(lng)}`}>
       </iframe>
     </>
   );
