0b24feac989577cd0227d0f32763161a49afdcbe
Hendrik Wichern <git@hendrikwichern.de>
2023-07-16 23:38:46 +0200

    css fix

--- src/components/blocks/Header.tsx
+++ src/components/blocks/Header.tsx
@@ -3,7 +3,7 @@ import { LoginButton } from '@/components/LoginButton';
 
 export function Header() {
   return (
-    <header className='sticky top-0 z-10 text-bg border-b border-text' style={{ backdropFilter: 'invert(1) grayscale(1)' }}>
+    <header className='sticky top-0 z-10 text-bg border-b border-text' style={{ backdropFilter: 'invert(1) grayscale(1)', WebkitBackdropFilter: 'invert(1) grayscale(1)' }}>
       <div className="container xl:max-w-full mx-auto px-1 md:px-3 items-center justify-between flex">
         <div className="my-4 md:mv-6 text-xl leading-tight md:leading-normal tracking-wide">
           <Link className="inline-block font-bold bg-text px-2" href="/">
