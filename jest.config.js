f6f4b7fb8f3f0a8aa9f44f57fb5a234b7dfcccc1
Hendrik Wichern <git@hendrikwichern.de>
2023-08-29 15:35:22 +0200

    updated checks

--- jest.config.js
+++ jest.config.js
@@ -1,5 +1,5 @@
 // eslint-disable-next-line @typescript-eslint/no-var-requires
-import * as nextJest from 'next/jest';
+const nextJest = require('next/jest');
 
 const createJestConfig = nextJest({
   // Provide the path to your Next.js app to load next.config.js and .env files in your test environment
