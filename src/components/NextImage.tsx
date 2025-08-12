ac33d7043e7e40a018631008c76633544df20f0b
Hendrik Wichern <git@hendrikwichern.de>
2025-08-12 14:08:24 +0200

    refactor next image

--- src/components/NextImage.tsx
+++ src/components/NextImage.tsx
@@ -6,24 +6,13 @@ import { useState } from 'react';
 
 import clsxm from '@/lib/clsxm';
 
-type NextImageProps = {
-  useSkeleton?: boolean;
+interface NextImageProps extends ImageProps {
   imgClassName?: string;
   blurClassName?: string;
-  alt: string;
-} & (
-  | { width: string | number; height: string | number }
-  | { layout: 'fill'; width?: string | number; height?: string | number }
-) &
-  ImageProps;
+  useSkeleton?: boolean;
+};
 
-/**
- *
- * @description Must set width using `w-` className
- * @param useSkeleton add background with pulse animation, don't use it if image is transparent
- */
 export default function NextImage({
-  useSkeleton = false,
   src,
   width,
   height,
@@ -31,6 +20,7 @@ export default function NextImage({
   className,
   imgClassName,
   blurClassName,
+  useSkeleton,
   ...rest
 }: NextImageProps) {
   const [status, setStatus] = useState(useSkeleton ? 'loading' : 'complete');
